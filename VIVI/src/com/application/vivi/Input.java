7f0bedf155c4653d96b47b7a67c831c3ad442dc1
petiteaiueo@gmail.com <petiteaiueo@gmail.com@e611830a-b55e-3c95-4a2f-a7d675c80c1e>
2012-11-12 04:41:20 +0000

    speech to text api, tapi belum dicoba di hp. di emulator ga bisa

--- VIVI/src/com/application/vivi/Input.java
+++ VIVI/src/com/application/vivi/Input.java
@@ -1,17 +1,23 @@
 package com.application.vivi;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class Input extends Activity {
 
-	private String bhsOrg, bhsDest, kata;
+	private String bhsOrg, bhsDest, hasil;
 	private Button btnRekam, btnNext;
+	protected static final int RESULT_SPEECH = 1;
 	
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -34,8 +40,21 @@ public class Input extends Activity {
         btnRekam.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
-				// TODO: rekam
-				kata = "tes";
+				Intent intent = new Intent(
+						RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+				intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, "en-US");
+
+				try {
+					startActivityForResult(intent, RESULT_SPEECH);
+					hasil = "";
+				} catch (ActivityNotFoundException a) {
+					Toast t = Toast.makeText(getApplicationContext(),
+							"Ops! Your device doesn't support Speech to Text",
+							Toast.LENGTH_SHORT);
+					t.show();
+					hasil = "tes";
+				}
 			}
 		});
         
@@ -46,7 +65,7 @@ public class Input extends Activity {
 				nextScreen.setClass(getApplicationContext(), HasilTerjemahan.class);
 				nextScreen.putExtra("bhsOrg", bhsOrg);
 				nextScreen.putExtra("bhsDest", bhsDest);
-				nextScreen.putExtra("kata", kata);
+				nextScreen.putExtra("hasil", hasil);
 				startActivity(nextScreen);
 			}
 		});
@@ -57,4 +76,19 @@ public class Input extends Activity {
         getMenuInflater().inflate(R.menu.activity_input, menu);
         return true;
     }
+    
+    @Override
+	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+
+		switch (requestCode) {
+			case RESULT_SPEECH: {
+				if (resultCode == RESULT_OK && null != data) {
+					ArrayList<String> text = data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+					hasil = text.get(0);
+				}
+				break;
+			}
+		}
+	}
 }
