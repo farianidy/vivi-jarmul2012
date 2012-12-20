1fc55dd3b465e4c38e47a711a76b2de48f4ba18d
petiteaiueo@gmail.com <petiteaiueo@gmail.com@e611830a-b55e-3c95-4a2f-a7d675c80c1e>
2012-12-20 07:53:15 +0000

    Finale

--- trunk/VIVI/gen/com/application/vivi/R.java
+++ trunk/VIVI/gen/com/application/vivi/R.java
@@ -15,17 +15,27 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
-        public static final int icon=0x7f020002;
+        public static final int background1=0x7f020000;
+        public static final int background2=0x7f020001;
+        public static final int background3=0x7f020002;
+        public static final int background4=0x7f020003;
+        public static final int background5=0x7f020004;
+        public static final int ic_action_search=0x7f020005;
+        public static final int ic_launcher=0x7f020006;
+        public static final int icon=0x7f020007;
+        public static final int language_but=0x7f020008;
+        public static final int next_butt=0x7f020009;
+        public static final int nextt=0x7f02000a;
+        public static final int record_but=0x7f02000b;
+        public static final int spelling_but=0x7f02000c;
     }
     public static final class id {
-        public static final int buttonAturBahasa=0x7f080006;
+        public static final int buttonAturBahasa=0x7f080007;
         public static final int buttonBicara=0x7f080013;
-        public static final int buttonLanjutHasil=0x7f080005;
+        public static final int buttonLanjutHasil=0x7f080006;
         public static final int buttonLanjutInput=0x7f08000c;
-        public static final int buttonNext=0x7f080007;
-        public static final int buttonRekam=0x7f080004;
+        public static final int buttonNext=0x7f080000;
+        public static final int buttonRekam=0x7f080005;
         public static final int imageView01=0x7f08000e;
         public static final int lviewImages=0x7f080014;
         public static final int menu_settings=0x7f080015;
@@ -33,25 +43,26 @@ public final class R {
         public static final int spinnerBhsDest=0x7f08000b;
         public static final int spinnerBhsOrg=0x7f080009;
         public static final int textView1=0x7f08000f;
-        public static final int txtViewBhsDest=0x7f080001;
+        public static final int txtViewBhsDest=0x7f080002;
         public static final int txtViewBhsDest2=0x7f080011;
-        public static final int txtViewBhsOrg=0x7f080000;
+        public static final int txtViewBhsOrg=0x7f080001;
         public static final int txtViewBhsOrg2=0x7f080010;
         public static final int txtViewDari=0x7f080008;
-        public static final int txtViewHasil=0x7f080003;
+        public static final int txtViewHasil=0x7f080004;
         public static final int txtViewHasilTerjemahan=0x7f080012;
         public static final int txtViewKe=0x7f08000a;
-        public static final int txtViewRekam=0x7f080002;
+        public static final int txtViewRekam=0x7f080003;
     }
     public static final class layout {
-        public static final int activity_input=0x7f030000;
-        public static final int activity_menu_awal=0x7f030001;
-        public static final int activity_pilih_bahasa=0x7f030002;
-        public static final int listview_row=0x7f030003;
-        public static final int main=0x7f030004;
+        public static final int activity_awal=0x7f030000;
+        public static final int activity_input=0x7f030001;
+        public static final int activity_menu_awal=0x7f030002;
+        public static final int activity_pilih_bahasa=0x7f030003;
+        public static final int listview_row=0x7f030004;
+        public static final int main=0x7f030005;
     }
     public static final class menu {
-        public static final int activity_hasil_terjemahan=0x7f070000;
+        public static final int activity_awal=0x7f070000;
         public static final int activity_input=0x7f070001;
         public static final int activity_menu_awal=0x7f070002;
         public static final int activity_pilih_bahasa=0x7f070003;
@@ -68,6 +79,7 @@ public final class R {
         public static final int judul_ke=0x7f040005;
         public static final int judul_pilih_bahasa=0x7f040003;
         public static final int menu_settings=0x7f040001;
+        public static final int title_activity_awal=0x7f04000b;
     }
     public static final class style {
         public static final int AppTheme=0x7f060000;
